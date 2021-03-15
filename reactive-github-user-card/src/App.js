df4badf08c6a748a84f49b301fff1f423a0efbbe
Omarius Credle <ocredle@omariuss-mbp.home>
2021-03-14 22:39:02 -0400

    created user cards with axios and promise all to handle multiple api calls.

--- reactive-github-user-card/src/App.js
+++ reactive-github-user-card/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
+import ReactDom from 'react-dom'; 
+import axios from 'axios'; 
 
-function App() {
+// Each Card contains Header: Name; subheading: Username ; Profile Image Content: Location, Profile URL, Followers, Following, Bio
+// name = data.name ; username = data.login; profile pic = data.avatar_url; location: data.location url = data.url followers = data.followers bio = data.bio
+
+class App extends React.Component {
+
+  state = {
+    userData: {},
+    followers: []
+  }
+  
+  componentDidMount(){
+    Promise.all([
+      axios.get('https://api.github.com/users/credleo95'),
+      axios.get('https://api.github.com/users/credleo95/followers')
+    ])
+    .then(( response => {
+      console.log(response[0].data)
+      this.setState({
+        ...this.state, 
+        userData: response[0].data,
+        followers: response[1].data
+      })
+      console.log("response working")
+    }))
+    .catch((error => {
+      console.log("something went wrong", error)
+    }))
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Github User Cards are here!</h1>
+      {/* render cards */}
+      <div>
+        <h1>{this.state.userData.name}</h1>
+        <h2>{this.state.userData.login}</h2>
+        <img src={`${this.state.userData.avatar_url}`} alt="Omarius Headshot"/>
+      </div>
+      <div>
+        <h1> Followers: </h1>
+          <section>
+            {this.state.followers.map(follower => {
+              return(
+              <div>
+                 <h1>{follower.name}</h1>
+                 <h2>{follower.login}</h2>
+                 <img src={`${follower.avatar_url}`} alt="Follower Headshot"/>
+              </div>
+              )
+                      
+            })
+            }
+          </section>
+      </div>
+    
     </div>
   );
+}
 }
 
 export default App;
