fe3afaaf3be56fb4df7f8ff3e3eab0c974185264
Omarius Credle <omariuscredle@omariuss-air.home>
2021-05-10 01:55:05 -0400

    data pulled from api for user profile and follower.

--- react-user-card/src/App.js
+++ react-user-card/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react'; 
 import axios from 'axios'; 
 import Card from './Components/Card'; 
+import FollowerCard from './Components/FollowerCard'; 
 import './App.css';
 
 class App extends React.Component{
 state = {
-  cardData: []
+  cardData: [],
+  followerData: []
 }
 
 componentDidMount(){
-  axios.get('https://api.github.com/users/credleo95')
+  Promise.all([
+    axios.get('https://api.github.com/users/credleo95'),
+    axios.get('https://api.github.com/users/credleo95/followers')
+  ])
   .then((res => {
-console.log(res.data)
-this.setState({cardData: res.data});
+console.log(res[0].data)
+this.setState({cardData: res[0].data})
+console.log(res[1].data)
+this.setState({followerData: res[1].data})
   }))
   .catch((err => {
     console.log('something went wrong: ', err); 
@@ -25,6 +32,10 @@ this.setState({cardData: res.data});
       <div className="App">
         <h1>The Card component is below</h1>
         <Card data={this.state.cardData}/>
+        {this.state.followerData.map(follower => {
+          return <FollowerCard followerData={follower}/>
+        })}
+        
         
       </div>
     );
